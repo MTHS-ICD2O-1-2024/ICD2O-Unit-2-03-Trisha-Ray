00c407132cfaa2ec4bf7823147d7fe0031b99c34
default <trisha.ray@stu.ocsb.ca>
2025-03-18 17:02:41 +0000

    fixed JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Trisha Ray
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
 /**
  * Displays an alert with the message "Hello, World!"
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+
+  const streetName = document.getElementById("street-name").value
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your adress is: " + streetNumber+ "  " +streetName + "."
 }
